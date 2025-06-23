bc3239e8bc1de170f0923721a76a22c2bd676a38
Jesús Ferretti <jferrettiboke@gmail.com>
2025-06-23 17:12:53 +0200

    feat(next.config): add experimental server components external packages for Puppeteer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: [
+      "puppeteer-core",
+      "@sparticuz/chromium-min",
+    ],
+  },
 };
 
 export default nextConfig;
